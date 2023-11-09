502d767f1a9a757d35332a93753f90c6095e2f54
calvin d. woo <calvin.woo@ww.com>
2023-11-09 17:51:23 -0500

    getting there

--- nonml/teeny/lex.py
+++ nonml/teeny/lex.py
@@ -1,16 +1,34 @@
+import enum
+import sys
+
+
 class Lexer:
     def __init__(self, source):
-        pass
+        self.source = source + "\n"
+        self.cur_pos = -1
+        self.cur_char = ''
+        self.next_char()
+
+    @property
+    def length(self):
+        return len(self.source)
 
     def next_char(self):
-        pass
+        self.cur_pos += 1
+        if self.cur_pos >= self.length:
+            self.cur_char = '\0' # eof
+        else:
+            self.cur_char = self.source[self.cur_pos]
 
     def peek(self):
         """returns lookahead char"""
-        pass
+        if self.cur_pos + 1 >= self.length:
+            return '\0'
+        else:
+            return self.source[self.cur_pos + 1]
 
     def abort(self, message: str):
-        pass
+        sys.exit(f"lexing error: {message}")
 
     def skip_whitespace(self):
         pass
@@ -19,4 +37,60 @@ class Lexer:
         pass
 
     def get_token(self):
-        pass
+        token = None
+        if self.cur_char == '+':
+            token = Token(self.cur_char, TokenType.PLUS)
+        elif self.cur_char == '-':
+            token = Token(self.cur_char, TokenType.MINUS)
+        elif self.cur_char == '*':
+            token = Token(self.cur_char, TokenType.ASTERISK)
+        elif self.cur_char == '/':
+            token = Token(self.cur_char, TokenType.SLASH)
+        elif self.cur_char == '\n':
+            token = Token(self.cur_char, TokenType.NEWLINE)
+        elif self.cur_char == '\0':
+            token = Token('', TokenType.EOF)
+        else:
+            # dunno
+            self.abort(f"unknown token {self.cur_char}")
+
+        self.next_char()
+        return token
+
+
+class Token:
+    def __init__(self, text, kind):
+        self.text = text
+        self.kind = kind
+
+
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    # keywords
+    LABEL = 4
+    GOTO = 5
+    PRINT = 6
+    INPUT = 7
+    LET = 8
+    IF = 9
+    THEN = 10
+    ENDIF = 11
+    WHILE = 12
+    REPEAT = 13
+    ENDWHILE = 14
+	# algebraic ops
+    EQ = 15
+    PLUS = 16
+    MINUS = 17
+    ASTERISK = 18
+    SLASH = 19
+    EQEQ = 20
+    NOTEQ = 21
+    LT = 22
+    LTEQ = 23
+    GT = 24
+    GTEQ = 25
---
+++ nonml/teeny/teeny.py
@@ -0,0 +1,15 @@
+from lex import Lexer, TokenType
+
+
+def main():
+    # source = "LET foobar = 123"
+    source = "+- */"
+    lexer = Lexer(source)
+
+    token = lexer.get_token()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.get_token()
+
+if __name__ == "__main__":
+    main()
