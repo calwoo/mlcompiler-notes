78d2fa890d6641935d9edd37dcac872e2042d032
calvin d. woo <calvin.woo@ww.com>
2023-11-09 17:35:50 -0500

    writing a small lexer

---
+++ nonml/teeny/lex.py
@@ -0,0 +1,22 @@
+class Lexer:
+    def __init__(self, source):
+        pass
+
+    def next_char(self):
+        pass
+
+    def peek(self):
+        """returns lookahead char"""
+        pass
+
+    def abort(self, message: str):
+        pass
+
+    def skip_whitespace(self):
+        pass
+
+    def skip_comment(self):
+        pass
+
+    def get_token(self):
+        pass
