c88f2961ef41627def475a1ffcf2a7e2cc6134fa
calvin d. woo <calvin.d.woo@gmail.com>
2023-11-19 16:51:29 -0500

    bump to next comp

--- nonml/cpprefresh/hw3/geometry.cpp
+++ nonml/cpprefresh/hw3/geometry.cpp
@@ -27,6 +27,59 @@ void Point::setY(const int new_y)
     y = new_y;
 }
 
+PointArray::PointArray()
+{
+    size = 0;
+    points = new Point[0];
+}
+
+PointArray::PointArray(const Point ppts[], const int ssize)
+{
+    size = ssize;
+    points = new Point[ssize];
+    for (int i = 0; i < ssize; i++) {
+        points[i] = ppts[i];
+    }
+}
+
+PointArray::PointArray(const PointArray& pvref)
+{
+    size = pvref.size;
+    points = new Point[size];
+    for (int i = 0; i < size; i++) {
+        points[i] = pvref.points[i];
+    }
+}
+
+PointArray::~PointArray()
+{
+    delete [] points;
+}
+
+void PointArray::resize(int new_size)
+{
+    Point* new_pts = new Point[new_size];
+    int min_size = (new_size > size ? size : new_size);
+    for (int i = 0; i < min_size; i++) {
+        new_pts[i] = points[i];
+    }
+    delete [] points;
+    size = new_size;
+    points = new_pts;
+}
+
+void PointArray::clear()
+{
+    resize(0);
+}
+
+void PointArray::pushBack(const Point& p)
+{
+    resize(size + 1);
+    points[size - 1] = p;
+}
+
+
 int main()
 {
     Point origin = Point();
--- nonml/cpprefresh/hw3/geometry.h
+++ nonml/cpprefresh/hw3/geometry.h
@@ -11,3 +11,25 @@ public:
     void setX(const int new_x);
     void setY(const int new_y);
 };
+
+class PointArray
+{
+private:
+    int size;
+    Point *points;
+    void resize(int size);
+
+public:
+    PointArray();
+    PointArray(const Point pts[], const int size);
+    PointArray(const PointArray& pv);
+    ~PointArray();
+
+    void clear();
+    int getSize() const { return size; }
+    void pushBack(const Point& p);
+    void insert(const int pos, const Point& p);
+    void remove(const int pos);
+    Point* get(const int pos);
+    const Point* get(const int pos) const;
+};
