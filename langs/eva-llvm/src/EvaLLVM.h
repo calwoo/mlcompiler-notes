d1406bac2da8c864c659caf5c8f06376dae396e2
calvin d. woo <calvin.d.woo@gmail.com>
2023-11-23 15:33:17 -0500

    add main function

--- langs/eva-llvm/src/EvaLLVM.h
+++ langs/eva-llvm/src/EvaLLVM.h
@@ -5,6 +5,7 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 
 class EvaLLVM {
     public:
@@ -17,6 +18,7 @@ class EvaLLVM {
 
             // compile to LLVM-IR
             // compile(ast);
+            compile();
 
             // print generated code
             module->print(llvm::outs(), nullptr);
@@ -32,11 +34,69 @@ class EvaLLVM {
             builder = std::make_unique<llvm::IRBuilder<>>(*ctx);
         }
 
+        void compile() {
+            // create main function
+            fn = createFunction(
+                "main", 
+                llvm::FunctionType::get(builder->getInt32Ty(), false)
+            );
+
+            // compile main body
+            auto result = gen(/* ast */);
+
+            // cast to i32 and return from main
+            auto i32Result = builder->CreateIntCast(result, builder->getInt32Ty(), true);
+            builder->CreateRet(i32Result);
+        }
+
+        /**
+         * main compile loop
+        */
+        llvm::Value* gen(/* exp */) {
+            return builder->getInt32(42);
+        }
+
+        llvm::Function* createFunction(const std::string& fnName,
+                                       llvm::FunctionType* fnType) {
+            // function prototype might already exist
+            auto fn = module->getFunction(fnName);
+            
+            if (fn == nullptr) {
+                // if null, allocate
+                fn = createFunctionProto(fnName, fnType);
+            }
+
+            createFunctionBlock(fn);
+            return fn;
+        }
+
+        llvm::Function* createFunctionProto(const std::string& fnName,
+                                            llvm::FunctionType* fnType) {
+            auto fn = llvm::Function::Create(fnType, llvm::Function::ExternalLinkage, 
+                                             fnName, *module);
+            verifyFunction(*fn);
+            return fn;
+        }
+
+        void createFunctionBlock(llvm::Function* fn) {
+            auto entry = createBB("entry", fn);
+            builder->SetInsertPoint(entry);
+        }
+
+        llvm::BasicBlock* createBB(std::string name, llvm::Function* fn = nullptr) {
+            return llvm::BasicBlock::Create(*ctx, name, fn);
+        }
+
         void saveModuleToFile(const std::string& fileName) {
             std::error_code errorCode;
             llvm::raw_fd_ostream outLL(fileName, errorCode);
             module->print(outLL, nullptr);
         }
+
+        /**
+         * current function
+        */
+        llvm::Function* fn;
 
         /**
          * global LLVM context, owns and manages core "global" data of LLVM's
